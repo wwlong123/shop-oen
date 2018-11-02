5bb89f7b6273d68c95a168d4096024fbd6bb4911
hjw <742917030@qq.com>
2018-11-02 15:37:57 +0800

    建伟    （上传配置文件）！

--- src/main/java/com/jk/ShopOenApplication.java
+++ src/main/java/com/jk/ShopOenApplication.java
@@ -1,9 +1,13 @@
 package com.jk;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource(value = {"classpath:shop-oenhjw.xml"})
+@MapperScan("com.jk.dao")
 public class ShopOenApplication {
 
     public static void main(String[] args) {
